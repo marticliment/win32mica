9a76ac5d71f73a95ea215518b22a90ea42a731cd
Martí Climent <marticlilop@gmail.com>
2023-01-29 10:41:56 +0100

    clarify tkinter examples (#13)

--- examples/Tkinter.py
+++ examples/Tkinter.py
@@ -10,7 +10,7 @@ except ImportError:
 
 app=tk.Tk()
 app.title("Tk Dark")
-app.configure(bg="#000000")
+app.configure(bg="#000000") # Please use BLACK as background color, otherwhise render issues might appear
 app.wm_attributes("-transparent", "#000000")
 app.update()
 HWND=windll.user32.GetParent(app.winfo_id())
@@ -19,8 +19,8 @@ mc.ApplyMica(HWND, ColorMode=mc.MICAMODE.DARK)
 
 app2=tk.Tk()
 app2.title("Tk Light")
-app2.configure(bg="#000000")
-app2.wm_attributes("-transparent", "#000000")
+app2.configure(bg="#ffffff") # Please use WHITE as background color, otherwhise render issues might appear
+app2.wm_attributes("-transparent", "#ffffff")
 app2.update()
 HWND=windll.user32.GetParent(app2.winfo_id())
 mc.ApplyMica(HWND, ColorMode=mc.MICAMODE.LIGHT)
