d13e53a2752f7aa928fbb0dfaadfa771ba8fc14a
小白云 <71159641+littlewhitecloud@users.noreply.github.com>
2023-08-31 19:01:20 +0800

    Update __init__.py

--- src/win32mica/__init__.py
+++ src/win32mica/__init__.py
@@ -1,24 +1,26 @@
 import ctypes, sys, threading, time, winreg
 
-class MICAMODE():
+
+class MICATHEME:
     DARK = 1
     LIGHT = 0
     AUTO = 2
 
-class MicaMode():
-    DARK = 1
-    LIGHT = 0
-    AUTO = 2
+
+class MICATYPE:
+    ALT = 1
+    DEFAULT = 0
 
 
 debugging = False
+
 
 def readRegedit(aKey, sKey, default, storage=winreg.HKEY_CURRENT_USER):
     registry = winreg.ConnectRegistry(None, storage)
     reg_keypath = aKey
     try:
         reg_key = winreg.OpenKey(registry, reg_keypath)
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         return default
     except Exception as e:
         print(e)
@@ -29,29 +31,40 @@ def readRegedit(aKey, sKey, default, storage=winreg.HKEY_CURRENT_USER):
             value_name, value, _ = winreg.EnumValue(reg_key, i)
             if value_name == sKey:
                 return value
-        except OSError as e:
+        except OSError:
             return default
         except Exception as e:
             print(e)
             return default
 
+
 def nullFunction():
     pass
-    
-def ApplyMica(HWND: int, ColorMode: bool = MicaMode.LIGHT, onThemeChange = nullFunction) -> int:
-    """Apply the new mica effect on a window making use of the hidden win32api and return an integer depending on the result of the operation
-    
+
+
+def ApplyMica(
+    HWND: int,
+    ColorMode: bool = MICATHEME.LIGHT,
+    MicaType: bool = MICATYPE.DEFAULT,
+    onThemeChange=nullFunction,
+) -> int:
+    """
+    Apply the new mica effect on a window making use of the hidden win32api and return an integer depending on the result of the operation
+
     Keyword arguments:
     HWND -- a handle to a window (it being an integer value)
-    ColorMode -- MicaMode.DARK or MicaMode.LIGHT, depending on the preferred UI theme. A boolean value can also be passed, True meaning Dark and False meaning Light
-    onThemeChange -- a function to call when the system theme changes. Will be called only if ColorMode is set to MicaMode.AUTO
+    ColorMode -- MICATHEME.DARK or MICATHEME.LIGHT, depending on the preferred UI theme.
+        A boolean value can also be passed, True meaning Dark and False meaning Light
+    MicaType -- MICATYPE.DEFAULT or MICATYPE.ALT, choose to apply the default mica or the mica alt.
+        A boolean value can also be passed, True meaning ALT and False meaning DEFAULT
+    onThemeChange -- a function to call when the system theme changes. Will be called only if ColorMode is set to MICATHEME.AUTO
     """
     try:
         try:
             HWND = int(HWND)
         except ValueError:
             HWND = int(str(HWND), 16)
-        
+
         user32 = ctypes.windll.user32
         dwm = ctypes.windll.dwmapi
 
@@ -60,44 +73,51 @@ def ApplyMica(HWND: int, ColorMode: bool = MicaMode.LIGHT, onThemeChange = nullF
                 ("AccentState", ctypes.c_uint),
                 ("AccentFlags", ctypes.c_uint),
                 ("GradientColor", ctypes.c_uint),
-                ("AnimationId", ctypes.c_uint)
+                ("AnimationId", ctypes.c_uint),
             ]
 
         class WindowCompositionAttribute(ctypes.Structure):
             _fields_ = [
                 ("Attribute", ctypes.c_int),
                 ("Data", ctypes.POINTER(ctypes.c_int)),
-                ("SizeOfData", ctypes.c_size_t)
+                ("SizeOfData", ctypes.c_size_t),
             ]
 
         class _MARGINS(ctypes.Structure):
-            _fields_ = [("cxLeftWidth", ctypes.c_int),
-                        ("cxRightWidth", ctypes.c_int),
-                        ("cyTopHeight", ctypes.c_int),
-                        ("cyBottomHeight", ctypes.c_int)
-                        ]
+            _fields_ = [
+                ("cxLeftWidth", ctypes.c_int),
+                ("cxRightWidth", ctypes.c_int),
+                ("cyTopHeight", ctypes.c_int),
+                ("cyBottomHeight", ctypes.c_int),
+            ]
 
-        DWM_UNDOCUMENTED_MICA_ENTRY = 1029 # Undocumented MICA (Windows 11 22523-)
-        DWM_UNDOCUMENTED_MICA_VALUE = 0x01 # Undocumented MICA (Windows 11 22523-)
-        
-        DWM_DOCUMENTED_MICA_ENTRY = 38     # Documented MICA (Windows 11 22523+)
-        DWM_DOCUMENTED_MICA_VALUE = 0x02   # Documented MICA (Windows 11 22523+)
+        DWM_UNDOCUMENTED_MICA_ENTRY = 1029  # Undocumented MICA (Windows 11 22523-)
+        DWM_UNDOCUMENTED_MICA_VALUE = (
+            0x01 if MicaType == MICATYPE.DEFAULT else 0x04
+        )  # Undocumented MICA (Windows 11 22523-)
+
+        DWM_DOCUMENTED_MICA_ENTRY = 38  # Documented MICA (Windows 11 22523+)
+        DWM_DOCUMENTED_MICA_VALUE = (
+            0x02 if MicaType == MICATYPE.DEFAULT else 0x04
+        )  # Documented MICA (Windows 11 22523+)
         DWMW_USE_IMMERSIVE_DARK_MODE = 20
-        
 
         SetWindowCompositionAttribute = user32.SetWindowCompositionAttribute
-        DwmSetWindowAttribute = dwm.DwmSetWindowAttribute 
+        DwmSetWindowAttribute = dwm.DwmSetWindowAttribute
         DwmExtendFrameIntoClientArea = dwm.DwmExtendFrameIntoClientArea
-        
+
         MODE = 0x00
 
         def setMode():
             nonlocal MODE
             OldMode = -1
             while True:
-                CurrentMode = readRegedit(r"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", "AppsUseLightTheme", 0)
+                CurrentMode = readRegedit(
+                    r"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize",
+                    "AppsUseLightTheme",
+                    0,
+                )
                 if OldMode != CurrentMode:
-                    
                     OldMode = CurrentMode
                     if MODE == 0x01:
                         ModeToSet = 0x01
@@ -109,27 +129,48 @@ def ApplyMica(HWND: int, ColorMode: bool = MicaMode.LIGHT, onThemeChange = nullF
                             onThemeChange()
                         except:
                             pass
-                    DwmSetWindowAttribute(HWND, DWMW_USE_IMMERSIVE_DARK_MODE, ctypes.byref(ctypes.c_int(ModeToSet)), ctypes.sizeof(ctypes.c_int))
+                    DwmSetWindowAttribute(
+                        HWND,
+                        DWMW_USE_IMMERSIVE_DARK_MODE,
+                        ctypes.byref(ctypes.c_int(ModeToSet)),
+                        ctypes.sizeof(ctypes.c_int),
+                    )
                     time.sleep(0.5)
-                    DwmSetWindowAttribute(HWND, DWMW_USE_IMMERSIVE_DARK_MODE, ctypes.byref(ctypes.c_int(ModeToSet)), ctypes.sizeof(ctypes.c_int))
+                    DwmSetWindowAttribute(
+                        HWND,
+                        DWMW_USE_IMMERSIVE_DARK_MODE,
+                        ctypes.byref(ctypes.c_int(ModeToSet)),
+                        ctypes.sizeof(ctypes.c_int),
+                    )
                     time.sleep(0.5)
-                    DwmSetWindowAttribute(HWND, DWMW_USE_IMMERSIVE_DARK_MODE, ctypes.byref(ctypes.c_int(ModeToSet)), ctypes.sizeof(ctypes.c_int))
+                    DwmSetWindowAttribute(
+                        HWND,
+                        DWMW_USE_IMMERSIVE_DARK_MODE,
+                        ctypes.byref(ctypes.c_int(ModeToSet)),
+                        ctypes.sizeof(ctypes.c_int),
+                    )
                     time.sleep(0.5)
-                    DwmSetWindowAttribute(HWND, DWMW_USE_IMMERSIVE_DARK_MODE, ctypes.byref(ctypes.c_int(ModeToSet)), ctypes.sizeof(ctypes.c_int))
+                    DwmSetWindowAttribute(
+                        HWND,
+                        DWMW_USE_IMMERSIVE_DARK_MODE,
+                        ctypes.byref(ctypes.c_int(ModeToSet)),
+                        ctypes.sizeof(ctypes.c_int),
+                    )
                     time.sleep(0.5)
                 time.sleep(0.1)
-        
-        if ColorMode == MicaMode.DARK:
+
+        if ColorMode == MICATHEME.DARK:
             MODE = 0x01
-        elif ColorMode == MicaMode.LIGHT:
+        elif ColorMode == MICATHEME.LIGHT:
             MODE = 0x00
-        else: # ColorMode == MicaMode.AUTO
+        else:  # ColorMode == MicaMode.AUTO
             MODE = 0x02
-        
-        threading.Thread(target=setMode, daemon=True, name="win32mica: theme thread").start()
+
+        threading.Thread(
+            target=setMode, daemon=True, name="win32mica: theme thread"
+        ).start()
 
         if sys.platform == "win32" and sys.getwindowsversion().build >= 22000:
-
             Acp = AccentPolicy()
             Acp.GradientColor = int("00cccccc", base=16)
             Acp.AccentState = 5
@@ -141,7 +182,7 @@ def ApplyMica(HWND: int, ColorMode: bool = MicaMode.LIGHT, onThemeChange = nullF
             Wca.Data = ctypes.cast(ctypes.pointer(Acp), ctypes.POINTER(ctypes.c_int))
 
             Mrg = _MARGINS(-1, -1, -1, -1)
-            
+
             o = DwmExtendFrameIntoClientArea(HWND, ctypes.byref(Mrg))
             try:
                 o = SetWindowCompositionAttribute(HWND, Wca)
@@ -149,13 +190,23 @@ def ApplyMica(HWND: int, ColorMode: bool = MicaMode.LIGHT, onThemeChange = nullF
                 pass
 
             if sys.getwindowsversion().build < 22523:
-                return DwmSetWindowAttribute(HWND, DWM_UNDOCUMENTED_MICA_ENTRY, ctypes.byref(ctypes.c_int(DWM_UNDOCUMENTED_MICA_VALUE)), ctypes.sizeof(ctypes.c_int))
+                return DwmSetWindowAttribute(
+                    HWND,
+                    DWM_UNDOCUMENTED_MICA_ENTRY,
+                    ctypes.byref(ctypes.c_int(DWM_UNDOCUMENTED_MICA_VALUE)),
+                    ctypes.sizeof(ctypes.c_int),
+                )
             else:
-                return DwmSetWindowAttribute(HWND, DWM_DOCUMENTED_MICA_ENTRY, ctypes.byref(ctypes.c_int(DWM_DOCUMENTED_MICA_VALUE)), ctypes.sizeof(ctypes.c_int))    
+                return DwmSetWindowAttribute(
+                    HWND,
+                    DWM_DOCUMENTED_MICA_ENTRY,
+                    ctypes.byref(ctypes.c_int(DWM_DOCUMENTED_MICA_VALUE)),
+                    ctypes.sizeof(ctypes.c_int),
+                )
         else:
-            print(f"Win32Mica Error: {sys.platform} version {sys.getwindowsversion().build} is not supported")
+            print(
+                f"Win32Mica Error: {sys.platform} version {sys.getwindowsversion().build} is not supported"
+            )
             return 0x32
     except Exception as e:
-        print("Win32mica: "+str(type(e))+": "+str(e))
-
-
+        print("Win32mica: " + str(type(e)) + ": " + str(e))
